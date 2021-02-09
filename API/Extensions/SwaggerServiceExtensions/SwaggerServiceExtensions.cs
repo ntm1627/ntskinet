dda6a944ed9e4f7362704ac7b6037dcef74d66a4
Naod Agere <ntm1627>
2021-02-09 10:58:34 -0500

    End of Section 16

--- API/Extensions/SwaggerServiceExtensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions/SwaggerServiceExtensions.cs
@@ -8,11 +8,29 @@ namespace API.Extensions.SwaggerServiceExtensions
     {
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>
-    {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skinet API", Version = "v1" });
+           services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SkiNet API", Version = "v1"});
 
-    });
+                //this will create the authorize property in swagger, with a key in each methods
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
+            });
 
             return services;
         }
@@ -23,7 +41,7 @@ namespace API.Extensions.SwaggerServiceExtensions
             app.UseSwaggerUI(c =>
             {
                 c
-  .SwaggerEndpoint("/swagger/v1/swagger.json", "Skinet API v1");
+                 .SwaggerEndpoint("/swagger/v1/swagger.json", "Skinet API v1");
             });
 
             return app;
